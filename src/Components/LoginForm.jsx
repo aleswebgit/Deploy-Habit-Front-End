e1f12ae2539b8dfc54a75c4561a9553a6f6a72ec
JessiJess <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-06-23 11:09:36 +0200

    added styles to the login form component

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -7,36 +7,36 @@ function LoginForm(){
     const navigate = useNavigate();
 
     return(
-        <form  className=" bg-[#F8D1B4] w-80 h-[406px] rounded-[52px] m-auto ">
-            <img
-                className="flex m-auto"
-				src = {Picprofile}
-				alt = 'Picprofile'/>
-            <div>
-                <input 
-                    className="rounded-3xl text-center p-3 px-11 m-auto flex my-2"
-                    type= 'text' 
-                    id='name' 
-                    name='name' 
-                    placeholder = 'username'
-                />
-            </div>
+        <form  className=" bg-[#F8D1B4] w-80 h-[406px] rounded-[52px] m-auto my-3">
+                <img
+                    className="flex m-auto"
+                    src = {Picprofile}
+                    alt = 'Picprofile'/>
+                <div>
+                    <input 
+                        className="rounded-3xl text-center p-3 px-11 m-auto flex my-2"
+                        type= 'text' 
+                        id='name' 
+                        name='name' 
+                        placeholder = 'username'
+                    />
+                </div>
 
-            <div>
-                <input 
-                    className="rounded-3xl p-3 text-center px-11 m-auto flex my-2"
-                    type = 'email' 
-                    id='name' 
-                    name='name' 
-                    placeholder = 'email'
-                /> 
-            </div>
-                <button 
-                    class="bg-[#D45C33] rounded-3xl text-center text-white px-9 p-2 m-auto flex"
-                    type='submit' 
-                    onClick={() => navigate("/home")}>
-                    Login
-                    </button>
+                <div>
+                    <input 
+                        className="rounded-3xl p-3 text-center px-11 m-auto flex"
+                        type = 'email' 
+                        id='name' 
+                        name='name' 
+                        placeholder = 'email'
+                    /> 
+                </div>
+                    <button 
+                        class="flex bg-[#D45C33] rounded-3xl text-center text-white px-9 p-2 m-auto my-8"
+                        type='submit' 
+                        onClick={() => navigate("/home")}>
+                        Login
+                        </button>
         </form>
     
     );
