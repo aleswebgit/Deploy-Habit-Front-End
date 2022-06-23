693080e2741f3b6f3c319ebecfdf2fe8f08fe766
JessiJess <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-06-23 09:58:24 +0200

    added the structure for loginform component

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Picprofile from '../media/picprofile.png';
 
 function LoginForm(){
+
+    const navigate = useNavigate();
+
     return(
-    <div>LoginForm</div>
+        <form>
+            <img
+				src = {Picprofile}
+				alt = 'Picprofile'/>
+            <div>
+                <input 
+                    type= 'text' 
+                    id='name' 
+                    name='name' 
+                    placeholder = 'username'
+                />
+            </div>
+
+            <div>
+                <input 
+                    type = 'email' 
+                    id='name' 
+                    name='name' 
+                    placeholder = 'email'
+                /> 
+            </div>
+                <button 
+                    type='submit' 
+                    onClick={() => navigate("/home")}>
+                    Login
+                    </button>
+        </form>
+    
     );
+    
 }
 
-export default LoginForm
+export default LoginForm;
+
