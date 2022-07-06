420d063ac758bb74d456e19f62260baef23c25b8
DamarisTeoc <teoc.918@gmail.com>
2022-07-06 15:13:38 +0200

    route section

--- src/routes/RoutesConfig.jsx
+++ src/routes/RoutesConfig.jsx
@@ -3,6 +3,7 @@ import Home from '../Views/Home';
 import Landing from '../Views/Landing';
 import Login from '../Views/Login';
 import Register from '../Views/Register';
+import Section from '../Views/Section';
 
 
 function RoutesConfig(){
@@ -11,7 +12,6 @@ function RoutesConfig(){
     <Router>
 
         <Link to="/"></Link>
-        <Link to="/home">Home</Link>
         <Link to="/test">Test</Link>
         <Link to="/landing">Landing</Link>
         <Link to="/login">Login</Link>
@@ -19,11 +19,10 @@ function RoutesConfig(){
 
         <Routes>
             <Route path="/" element={ <Home/> } />
-            <Route path="/home" element={ <Home/> } />
             <Route path="/landing" element={ <Landing/> } />
             <Route path="/login" element={ <Login/> } />
             <Route path="/register" element={ <Register/> } />
-
+            <Route path="/section" element={ <Section/> } />
         </Routes>
 
     </Router>
