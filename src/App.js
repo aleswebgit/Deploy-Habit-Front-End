28a90dc5daf79a4c65f68a3474cb65566951037b
itsberriver <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-07-07 10:34:11 +0200

    deleted navbar in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import React from 'react';
 // import RoutesConfig from './routes/RoutesConfig'
-import Navbar from './Components/Navbar';
 
 function App() {
 
   return (
     <div>
       {/* <RoutesConfig /> */}
-      <Navbar/>
     </div>
   );
 }
