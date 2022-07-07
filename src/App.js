2ecbecf8998d544f2e7e023cca6a58c2eb8211d4
itsberriver <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-07-07 10:41:08 +0200

    linked routes config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
+import RoutesConfig from './routes/RoutesConfig.jsx';
 
 
 function App() {
 
   return (
     <div>
-      {/* <RoutesConfig /> */}
+      <RoutesConfig />
     </div>
   );
 }
