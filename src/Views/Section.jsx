77ccbb71e2e448def977a8b8c6ea57cb2b7c28f5
itsberriver <jessicamejiaosorio@AirdeJessiJess.home>
2022-07-08 09:28:44 +0200

    changed styles

--- src/Views/Section.jsx
+++ src/Views/Section.jsx
@@ -1,7 +1,17 @@
+import SectionsComponent from '../Components/SectionsComponent'
+
 
 const Section = () => {
   return (
-    <div>Section</div>
+    <>
+      <h1>Nutrición</h1>
+      <SectionsComponent text='Subapartado uno'/>
+      <SectionsComponent text='Subapartadp dos '/>
+      <SectionsComponent text='Subapartadp tres'/>
+      <SectionsComponent text='Subapartadp cuatro'/>
+      <SectionsComponent text='Subapartadp cinco'/>
+    </>
+
   )
 }
 
