c3316102af7a4a639adbd430ba60aa27ab7b86b9
itsberriver <jmejiaos713@gmail.com>
2022-07-11 14:45:20 +0200

    reordered components when desktop responsive

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -22,15 +22,18 @@ const Home = () => {
           <>
             <Navbar/>
             <h1 className='flex justify-center text-5xl text-center py-2 m-8 text-[#BC4E2A]'>Sobre el proyecto</h1>
-            <div className="bg-[#E57A56] rounded-3xl p-5 m-4 sm:w-[800px] sm:h- sm:m-9 text-[#FDF6EC] font-medium">
-              <p className="text-center ">
+            <div className="sm:flex ">
+
+              <div className="bg-[#E57A56] rounded-3xl p-5 m-4 sm:max-w-[700px] sm:m-9 text-[#FDF6EC] font-medium">
+                <p className="text-center ">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor amet, blandit xfdvds molestie viverra nisi. Tellus nam lacus csdcametus, a, enim habitasse neque ut aliquam. Adipiscing tempu askcbaskcb blandit xfdvds molestie viverra nisi. Tellus nam.
-              </p>
-              <a href="" className="block mx-auto w-fit color-[#ffff]">
+                </p>
+                <a href="" className="block mx-auto w-fit color-[#ffff]">
                 Ver más...
-              </a>
+                </a>
+              </div>
+              <Category/>
             </div>
-            <Category/>
             <Advice/>
           </>
         )}
