4876f26e9060c9a3f28c98f1f3a78df7e2633ea8
itsberriver <jmejiaos713@gmail.com>
2022-07-11 13:00:14 +0200

    added styles in home

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -39,12 +39,12 @@ const Category = () => {
     <section className='grid grid-cols-2 gap-4 p-6'>
       {categories.map( category => {
         return (                        
-          <a key={category.name} href="/section" className='flex justify-center'>
+          <a key={category.name} href="/section" className='flex justify-center py-3'>
             <div className={`${category.bgColor} flex flex-col items-center justify-center w-[130px] aspect-square rounded-3xl`}>
-              <div className="flex item-center justify-center">
+              <div className="flex item-center justify-center ">
                 {category.icon}
               </div>
-              <p className={`${category.color} font-['Titan one']`}>
+              <p className={`${category.color} m-2 font-['Titan one']`}>
                 {category.name}
               </p>
             </div>
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -21,13 +21,13 @@ const Home = () => {
         : (
           <>
             <Navbar/>
-            <h1 className='flex justify-center text-[#BC4E2A]'>Sobre el proyecto</h1>
-            <div className="bg-[#E57A56] rounded-3xl p-9 text-[#FDF6EC] font-medium space-y-4">
+            <h1 className='flex justify-center m-8 text-[#BC4E2A]'>Sobre el proyecto</h1>
+            <div className="bg-[#E57A56] rounded-3xl p-5 m-4 sm:m-9 text-[#FDF6EC] font-medium">
               <p className="text-center ">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor amet, blandit xfdvds molestie viverra nisi. Tellus nam lacus csdcametus, a, enim habitasse neque ut aliquam. Adipiscing tempu askcbaskcb
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor amet, blandit xfdvds molestie viverra nisi. Tellus nam lacus csdcametus, a, enim habitasse neque ut aliquam. Adipiscing tempu askcbaskcb blandit xfdvds molestie viverra nisi. Tellus nam.
               </p>
-              <a href="" className="block mx-auto w-fit">
-                Ver mas...
+              <a href="" className="block mx-auto w-fit  underline-offset-1">
+                Ver más...
               </a>
             </div>
             <Category/>
