9b2e57613de1cb22bd48086b4a675847ef93441a
itsberriver <jmejiaos713@gmail.com>
2022-07-11 18:31:47 +0200

    linked elements in tje dropdown menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,11 +2,11 @@ import {CgMenuBoxed} from 'react-icons/cg'
 import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React,{useState} from 'react'
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
 
 function Navbar(){
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const [dropdown, setDropdown] = useState(false)
 
@@ -14,21 +14,19 @@ function Navbar(){
     setDropdown(!dropdown)
   }
 
-
-    
   return(
     
-    <nav className='bg-[#FDF6EC] w-full flex relative justify-between items-center mx-auto px-8 h-20 '>
-      <Dropdown isOpen = {dropdown} toggle = {openCloseDropdown} className = 'border-transparent' >
-        <DropdownToggle className = 'bg-transparent border-0'>
+    <nav className='w-full flex relative justify-between items-center mx-auto px-8 h-20 '>
+      <Dropdown isOpen = {dropdown} toggle = {openCloseDropdown} className = 'border-transparent ' >
+        <DropdownToggle className = 'bg-transparent border-0 '>
           <CgMenuBoxed className = 'text-[#BC4E2A] display-block h-[48px] w-[48px] '/>
         </DropdownToggle >
     
         <DropdownMenu>
-          <DropdownItem>Home</DropdownItem>
-          <DropdownItem>Registrar Nuevo</DropdownItem>
-          <DropdownItem>Categorías</DropdownItem>
-          <DropdownItem>Mi perfil</DropdownItem>
+          <DropdownItem onClick={() => navigate('/')}>Home</DropdownItem>
+          <DropdownItem onClick={() => navigate('/admin-register')}>Registrar Nuevo</DropdownItem>
+          <DropdownItem onClick={() => navigate('/')}>Categorías</DropdownItem>
+          <DropdownItem onClick={() => navigate('/')}>Mi perfil</DropdownItem>
         </DropdownMenu>
       </Dropdown>
       <a href="admin-register" type="button" className="py-2 px-3 hover:underline rounded-full relative">
