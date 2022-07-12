edcd3aad73762252f276c00284a87c232546fd6e
DamarisTeoc <teoc.918@gmail.com>
2022-07-12 16:08:00 +0200

    try print sections of category

--- src/Components/SectionButton.jsx
+++ src/Components/SectionButton.jsx
@@ -1,5 +1,8 @@
 
-function SectionButton({text,onClick}){
+function SectionButton({
+  text,
+  onClick
+}){
   return(
     <button 
       className = "flex bg-[#F8D1B4] rounded-3xl w-[336px] justify-center text-[#E57A56] p-4 m-auto my-8 mt-3 mb-3"
--- src/Views/Category.jsx
+++ src/Views/Category.jsx
@@ -1,15 +1,67 @@
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import axios from '../api/axios'
+import Navbar from '../Components/Navbar'
 import SectionButton from '../Components/SectionButton'
 
 
 const Category = () => {
+  const selectCategory = useParams().id
+  const navigate = useNavigate()
+  // const [category, setCategory] = useState({})
+  const [filter, setFilter] = useState('sueño')
+  const [sections, setSections] = useState([])
+
+  selectCategory === 2 ? setFilter('nutricion') 
+    : selectCategory === 3
+      ? setFilter('salud mental')
+      : selectCategory === 4
+        ? setFilter('deporte')
+        : setFilter('sueño')
+  
+  const sectionsOfCategoryRequest = async () =>{
+    try {
+      const response = await axios.get('/sections',
+        {
+          headers:{
+            'Content-Type' : 'application/json',
+            'Access-Control-Allow-Origin' : '*',
+            'x-access-token': localStorage.getItem('token')
+          },
+        }
+      ).then(response => {
+        // setCategory(response.data)
+        console.log(response.data)
+        const filterSections = response.data.filter(section => section.category === filter)
+        setSections(filterSections)
+      })
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  useEffect(()=>{
+    sectionsOfCategoryRequest()
+  },[])
+
   return (
     <>
-      <h1>Nutrición</h1>
-      <SectionButton text='Subapartado uno'/>
+      <Navbar />
+      <h1>Sueño</h1>
+      {sections.map(section => {
+        {/* return (
+          <div key={section.id}>
+            <SectionButton name={section.name} />
+          </div>
+        ) */}
+        return <SectionButton key={selectCategory} name={section.name} onClick={() => navigate('/section/' + section.id)} />
+      })}
+      {/* <SectionButton text='Subapartado uno'/>
       <SectionButton text='Subapartadp dos '/>
       <SectionButton text='Subapartadp tres'/>
       <SectionButton text='Subapartadp cuatro'/>
-      <SectionButton text='Subapartadp cinco'/>
+      <SectionButton text='Subapartadp cinco'/> */}
     </>
   )
 }
