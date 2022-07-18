26550ec9d202c0440dbe5ff08c5ad783698eb4f1
Ales <aleswebgit@gmail.com>
2022-07-18 16:20:04 +0200

    fix bg categories color

--- src/Components/CategoryDesktopButton.jsx
+++ src/Components/CategoryDesktopButton.jsx
@@ -1,7 +1,7 @@
 const CategoryDesktopButton = ({category}) => {
   return (
     <div className="hidden lg:block box-border h-80 w-100 p-10 space-y-7 border-2 border-[#BC4E2A] rounded-2xl shadow-md shadow-[#BC4E2A]/30">
-      <div className={`${category.bgColor} flex flex-col items-center justify-center bg-[#F8D1B4] w-[130px] aspect-square rounded-[35px] mx-auto`}>
+      <div className={`${category.bgColor} flex flex-col items-center justify-center w-[130px] aspect-square rounded-[35px] mx-auto`}>
         <div className="flex item-center justify-center ">
           {category.icon}
         </div>
