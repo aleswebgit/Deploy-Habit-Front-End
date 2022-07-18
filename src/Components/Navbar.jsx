ef199327c58ac1695afd847dbfb341fa22a0b84f
ginkgo <alexvid33@gmail.com>
2022-07-18 17:16:27 +0200

    fix navbar dropdown redirection to categories

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -35,10 +35,10 @@ function Navbar(){
         <DropdownMenu aria-expanded aria-label='menu desplegable'>
           <DropdownItem aria-level={1} onClick={() => navigate('/')}>Página principal</DropdownItem>
           <DropdownItem aria-level={2} onClick={() => navigate('/admin-register')}>Alta nueva en el sistema</DropdownItem>
-          <DropdownItem aria-level={3} onClick={() => navigate('/category/:1')}>Sueño</DropdownItem>
-          <DropdownItem aria-level={4} onClick={() => navigate('/category/:2')}>Nutrición</DropdownItem>
-          <DropdownItem aria-level={5} onClick={() => navigate('/category/:3')}>Deporte</DropdownItem>
-          <DropdownItem aria-level={6} onClick={() => navigate('/category/:4')}>Salud Mental</DropdownItem>
+          <DropdownItem aria-level={3} onClick={() => navigate('/category/1')}>Sueño</DropdownItem>
+          <DropdownItem aria-level={4} onClick={() => navigate('/category/2')}>Nutrición</DropdownItem>
+          <DropdownItem aria-level={5} onClick={() => navigate('/category/3')}>Deporte</DropdownItem>
+          <DropdownItem aria-level={6} onClick={() => navigate('/category/4')}>Salud Mental</DropdownItem>
           <DropdownItem aria-level={7} onClick={() => navigate('/about-us')}>Sobre Nosotros</DropdownItem>
         </DropdownMenu>
       </Dropdown>
