e62c47a79ef56225ea66653353ffdf6a9152735d
itsberriver <jmejiaos713@gmail.com>
2022-07-20 17:29:25 +0200

    styled text in section views

--- src/Views/Section.jsx
+++ src/Views/Section.jsx
@@ -34,11 +34,11 @@ const Section =() => {
   return (
     <>
       <Navbar />
-      <h1>{ section.title }</h1>
-      <p>{ section.category }</p>
+      <h1 className='flex justify-center text-5xl text-center py-2 m-8 text-[#BC4E2A] uppercase'>{ section.title }</h1>
+      <p className = "justify-center text-center text-[#E57A56]">{ section.category }</p>
       {section.text && section.text.map(element => {
-        if (element[0] === 'p') return <p key={uuid()}>{element[1]}</p>
-        if (element[0] === 'a') return <a href={element[2]} key={uuid()} className="block">{element[1]}</a>
+        if (element[0] === 'p') return <p className = "justify-center m-4" key={uuid()}>{element[1]}</p>
+        if (element[0] === 'a') return <a  href={element[2]} key={uuid()} className="block justify-center my-2 mx-4 decoration-solid">{element[1]}</a>
       })}
     </>
 
