c124c42bfba2b1d2b669fd489167db4d9e603e3b
itsberriver <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-07-22 11:25:48 +0200

    added a px margin to the column texts

--- src/Views/Section.jsx
+++ src/Views/Section.jsx
@@ -37,12 +37,12 @@ const Section =() => {
       <Navbar />
       <h1 className='flex justify-center text-5xl text-center md:mb-20 py-2 m-8 text-[#BC4E2A] uppercase'>{ section.title }</h1>
       {/* <p className = "justify-center text-center text-[#E57A56]">{ section.category }</p> */}
-      <div className="md:columns-2 	md:break-after-column md:m-6">
+      <div id="links" className="md:columns-2 md:px-2	md:break-after-column md:m-6">
 
         {section.text && section.text.map(element => {
         
           if (element[0] === 'p') return <p className = "justify-center	 m-4 mt-0" key={uuid()}>{element[1]}</p>
-          if (element[0] === 'a') return <a  href={element[2]} key={uuid()} className="block justify-center my-2 mx-4 decoration-solid">{element[1]}</a>
+          if (element[0] === 'a') return <a  href={element[2]} key={uuid()} className="block justify-center my-2 mx-4 decoration-solid font-bold outline-offset-3 text-[#BC4E2A]">{element[1]}</a>
         })}
       </div>
       
